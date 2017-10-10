e10aa275f696908e1986697990cbab37627e29d0
Daniel Hug <dhug@albany.edu>
2017-10-10 01:04:36 -0400

    Debuged errors in the sortList function

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ typedef struct logLine{
   char level[20];
   char timeStamp[20];
   char message[100];
+  struct logLine *next;
 } logLine_t;
 
 typedef struct logList {
@@ -96,3 +97,113 @@ void printLines(logList_t* l){
   }
 }
 //////ADD JESS'S OTHER TWO FUNCTIONS OR ELSE YOU'RE GOING TO DIE
+struct logList* mergeLists(struct logList* resultList, struct logList* inList){
+ 
+    struct logList* temp = resultList;
+    
+    while(resultList->next != NULL){
+        resultList = resultList->next;
+    }
+    resultList->next = inList;
+    return temp;
+}
+
+int listLength(struct logList* testList){
+    int counter = 0;
+    
+    while(testList != NULL){
+        counter++;
+        testList = testList->next;
+    }
+    
+    return counter;
+}
+
+struct logList* sortList(struct logList* inList){
+    
+    int switched = 0;
+    struct logLine *ptr1;
+    struct logLine *ptr2 = NULL;
+    struct logList *ptr3;
+    struct logList *ptr4 = NULL;
+    
+    
+    char year1[4]; char month1[2]; char date1[2]; char hours1[2]; char seconds1[2]; char minutes1[2];
+    char year2[4]; char month2[2]; char date2[2]; char hours2[2]; char seconds2[2]; char minutes2[2];
+    
+    
+    do{
+        switched = 0;
+        ptr1 = inList;
+        
+        while(ptr3->next != ptr2) {
+            
+            strcpy(year1, strtok(ptr1->timeStamp, "-"));
+            strcpy(month1, strtok(NULL, "-"));
+            strcpy(date1, strtok(NULL, "-"));
+            strcpy(hours1, strtok(NULL, " "));
+            strcpy(minutes1, strtok(NULL, ":"));
+            strcpy(seconds1, strtok(NULL, ":"));
+            
+            strcpy(year2, strtok((ptr1->next)->timeStamp, "-"));
+            strcpy(month2, strtok(NULL, "-"));
+            strcpy(date2, strtok(NULL, "-"));
+            strcpy(hours2, strtok(NULL, " "));
+            strcpy(minutes2, strtok(NULL, ":"));
+            strcpy(seconds2, strtok(NULL, ":"));
+            
+            if (strtol(year1) > strtol(year2)){
+                switcher(ptr3, ptr3->next);
+                switched = 1;
+            }
+            else if (strtol(year1) == strtol(year2)){
+                if(strtol(month1) > strtol(month2)){
+                    switcher(ptr3, ptr3->next);
+                    switched = 1;
+                }
+                else if (strtol(month1) == strtol(month2)){
+                    if(strtol(date1) > strtol(date2)){
+                        switcher(ptr3, ptr3->next);
+                        switched = 1;
+                    }
+                    else if(strtol(date1) == strtol(date2)){
+                        if(strtol(hours1) > strtol(hours2)){
+                            switcher(ptr3, ptr3->next);
+                            switched = 1;
+                        }
+                        else if(strtol(hours1) == strtol(hours2)){
+                            if(strtol(minutes1) > strtol(minutes2)){
+                                switcher(ptr3, ptr3->next);
+                                switched = 1;
+                            }
+                            else if (strtol(minutes1) == strtol(minutes2)){
+                                if(strtol(seconds1) > strtol(seconds2)){
+                                    switcher(ptr3, ptr3->next);
+                                    switched = 1;
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+            ptr3 = ptr3->next;
+        }
+        ptr4 = ptr3;
+    }
+    while(switched);
+    
+    return ptr3;
+}
+
+void switcher(struct logLine* a, struct logLine* b){
+    char temp1[20];
+    char temp2[20];
+    char temp3[20];
+    
+    temp1 = a->level; temp2 = a->timeStamp; temp3 = a->message;
+    a->level = b->level; a->timeStamp = b->timeStamp; a->message = b->message;
+    b->level = temp1; b->timeStamp = temp2; b->message = temp3;
+    
+} 
+
